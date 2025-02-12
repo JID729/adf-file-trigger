ffb1f43d1a94b4704e117a8a3b184c808ed2eb7d
JID729 <jeriel@ensombl.com>
2025-02-12 13:35:56 +0800

    Create function_app.py

---
+++ .github/workflows/function_app.py
@@ -0,0 +1,8 @@
+import logging
+import azure.functions as func
+
+app = func.FunctionApp()
+
+@app.event_grid_trigger(arg_name="azeventgrid")
+def EventGridTriggerBlob(azeventgrid: func.EventGridEvent):
+    logging.info('Python EventGrid trigger processed an event')
