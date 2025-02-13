a1228321ef69f3a06c6f838be3a987bb06d6adb0
JID729 <jeriel@ensombl.com>
2025-02-13 10:13:31 +0800

    Delete .github/workflows directory

--- .github/workflows/__init__.py
+++
@@ -1,5 +0,0 @@
-import logging
-import azure.functions as func
-
-def main(event: func.EventGridEvent):
-    logging.info(f"Event received: {event.get_json()}")
--- .github/workflows/function_app.py
+++
@@ -1,5 +0,0 @@
-import logging
-import azure.functions as func
-
-def main(event: func.EventGridEvent):
-    logging.info('Received Event Grid event: %s', event.get_json())
